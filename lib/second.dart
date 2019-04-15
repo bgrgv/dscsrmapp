adc5f07734d2e8934bdaf5e0a5dfab7be58dd315
bgrgv <blessin_george@srmuniv.edu.in>
2019-04-15 19:57:42 +0530

    Added Registrations Web View in TabScreen2.

--- lib/second.dart
+++ lib/second.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class TabScreen2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: new Text("Second"),
+      child: new WebView(
+        initialUrl: 'https://www.ishanchhabra.com/sample-reg.html',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
