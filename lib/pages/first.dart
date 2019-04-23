234ccaf85985d1c071926eff7fe1e7ec917878b0
smiraldr <42693530+smiraldr@users.noreply.github.com>
2019-04-23 19:26:31 +0530

    Carousel Added
    Carousel has been added with carousel_pro package . Changed files are first.dart and pubspec.yaml rest files were auto changed

--- lib/pages/first.dart
+++ lib/pages/first.dart
@@ -1,26 +1,28 @@
-
 import 'package:flutter/material.dart';
-import 'package:dscsrmapp/Menu_Mode.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class TabScreen1 extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: //new Text("First"),
-          new Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          RaisedButton(
-            color: Colors.red,
-            child: Text('Menu Mode'),
-            onPressed: () {
-              Navigator.of(context).push(
-                  MaterialPageRoute<Null>(builder: (BuildContext context) {
-                return new MenuMode();
-              }));
-            },
-          ),
+Widget imageCarousel = new Container(
+      height: 200.0,
+      child: new Carousel(
+        boxFit: BoxFit.cover,
+        images:[
+          new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+          new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
         ],
+        animationCurve: Curves.fastOutSlowIn,
+        animationDuration: Duration(milliseconds:1000),
+      ), 
+    );
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        body: new ListView(
+           children: <Widget>[
+                imageCarousel,
+           ]
+        ),
       ),
     );
   }
