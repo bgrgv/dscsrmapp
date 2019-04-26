5e62a06bc93f2f3ab993d2add05f863584917a60
ArtistBanda <chirag.11gomber4@gmail.com>
2019-04-27 03:32:18 +0530

    merged carousel and menu mode button

--- lib/pages/first.dart
+++ lib/pages/first.dart
@@ -1,28 +1,39 @@
+import 'package:dscsrmapp/Menu_Mode.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 
 class TabScreen1 extends StatelessWidget {
   @override
-Widget imageCarousel = new Container(
-      height: 200.0,
-      child: new Carousel(
-        boxFit: BoxFit.cover,
-        images:[
-          new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
-          new NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
-        ],
-        animationCurve: Curves.fastOutSlowIn,
-        animationDuration: Duration(milliseconds:1000),
-      ), 
-    );
+  Widget imageCarousel = new Container(
+    height: 200.0,
+    child: new Carousel(
+      boxFit: BoxFit.cover,
+      images: [
+        new NetworkImage(
+            'https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+        new NetworkImage(
+            'https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+      ],
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(milliseconds: 1000),
+    ),
+  );
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        body: new ListView(
-           children: <Widget>[
-                imageCarousel,
-           ]
-        ),
+        body: new ListView(children: <Widget>[
+          imageCarousel,
+          RaisedButton(
+            color: Colors.red,
+            child: Text('Menu Mode'),
+            onPressed: () {
+              Navigator.of(context).push(
+                  MaterialPageRoute<Null>(builder: (BuildContext context) {
+                return new MenuMode();
+              }));
+            },
+          ),
+        ]),
       ),
     );
   }
