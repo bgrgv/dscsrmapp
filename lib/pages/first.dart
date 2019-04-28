468b4a9f4f7f067656fbe9612e725e2f51681c07
smiraldr <42693530+smiraldr@users.noreply.github.com>
2019-04-28 18:57:11 +0530

    Card View Added
    Card View Added with 2 Templates. Follow The comments to replace and use code to get either formats. Still need to create a Constructor for East Card Generation.

--- lib/pages/first.dart
+++ lib/pages/first.dart
@@ -2,6 +2,7 @@ import 'package:dscsrmapp/eventmode.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 
+
 class TabScreen1 extends StatelessWidget {
   final Widget imageCarousel = new Container(
     height: 200.0,
@@ -22,12 +23,8 @@ class TabScreen1 extends StatelessWidget {
         body: new ListView(children: <Widget>[
           imageCarousel,
           Padding(
-            padding: const EdgeInsets.only(top: 80.0, bottom: 80.0),
-            child: new Text(
-              "Place for Cards...\nWork Pending :/",
-              style: TextStyle(fontSize: 30.0),
-              textAlign: TextAlign.center,
-            ),
+            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child:NewCardWidget() //changing this to NewCardWidget2 will give you second template
           ),
           Column(
             children: <Widget>[
@@ -56,3 +53,80 @@ class TabScreen1 extends StatelessWidget {
     );
   }
 }
+
+class NewCardWidget extends StatelessWidget { //Template 1
+  NewCardWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const ListTile(
+              leading: Icon(Icons.album),
+              title: Text('The Enchanted Nightingale'),
+              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+            ),
+            ButtonTheme.bar(
+              // make buttons use the appropriate styles for cards
+              child: ButtonBar(
+                children: <Widget>[
+                  FlatButton(  //Buttons if not needed remove it
+                    child: const Text('BUY TICKETS'),
+                    onPressed: () {/* ... */},
+                  ),
+                  FlatButton( //Buttons if not needed remove it
+                    child: const Text('LISTEN'),
+                    onPressed: () {/* ... */},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewCardWidget2 extends StatelessWidget { //template 2
+  NewCardWidget2({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+                    child: new Column(
+                      children: <Widget>[
+                        new Image.network('https://i.ytimg.com/vi/fq4N0hgOWzU/maxresdefault.jpg'),
+                        new Padding(
+                          padding: new EdgeInsets.all(7.0),
+                          child: new Row(
+                            children: <Widget>[
+                             new Padding(
+                               padding: new EdgeInsets.all(7.0),
+                               child: new Icon(Icons.thumb_up),
+                             ),
+                             new Padding(
+                               padding: new EdgeInsets.all(7.0),
+                               child: new Text('Like',style: new TextStyle(fontSize: 18.0),),
+                             ),
+                             new Padding(
+                               padding: new EdgeInsets.all(7.0),
+                               child: new Icon(Icons.comment),
+                             ),
+                             new Padding(
+                               padding: new EdgeInsets.all(7.0),
+                               child: new Text('Comments',style: new TextStyle(fontSize: 18.0)),
+                             )
+
+                            ],
+                          )
+                        )
+                      ],
+                    ),
+                  );
+  }
+}
+
